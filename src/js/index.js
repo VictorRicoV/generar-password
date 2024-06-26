3758099301e3928f539d57a10fd90122393fff2b
Victor <lessricc@gmail.com>
2024-06-26 17:13:18 +0200

    listo

--- src/js/index.js
+++ src/js/index.js
@@ -1,31 +1,26 @@
 // El styles lo importamos aquí para que se encargue Vite de compilar todo
-import "../scss/styles.scss";
+import '../scss/styles.scss';
 
-const labelElement = document.getElementById("label");
-const rangeElement = document.getElementById("range");
-const buttonElement = document.getElementById("generator");
-const finishedPassword = document.getElementById("password");
+const labelElement = document.getElementById('label');
+const rangeElement = document.getElementById('range');
+const buttonElement = document.getElementById('generator');
+const finishedPassword = document.getElementById('password');
 
-//const characterAvailable =
-//"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01234567890!@#$%^&*()_+-={}[]:;<>,.?/";
+const characterAvailable = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01234567890!@#$%^&*()_+-={}[]:;<>,.?/';
 
 const lengthValue = () => {
   labelElement.textContent = `LENGTH: ${rangeElement.value}`;
 };
 
-rangeElement.addEventListener("input", lengthValue);
+rangeElement.addEventListener('input', lengthValue);
 
-const password = (character) => {
-  let randomPassword = "";
+const password = () => {
+  let randomPassword = '';
   for (let i = 0; i < rangeElement.value; i++) {
-    const randomNumber = Math.floor(Math.random() * character.length);
-    randomPassword += character.charAt(randomNumber);
+    const randomNumber = Math.floor(Math.random() * characterAvailable.length);
+    randomPassword += characterAvailable.charAt(randomNumber);
   }
   finishedPassword.value = randomPassword;
 };
 
-buttonElement.addEventListener("click", () => {
-  password(
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01234567890!@#$%^&*()_+-={}[]:;<>,.?/"
-  );
-});
+buttonElement.addEventListener('click', password);
